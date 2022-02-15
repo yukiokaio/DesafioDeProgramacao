af2191885b4b1ce3b2bdf80721cc678ebe83721a
Kaio Yukio <34402495+yukiokaio@users.noreply.github.com>
2022-02-15 18:56:52 -0300

    Update Questao2.java

--- Questao2/Questao2.java
+++ Questao2/Questao2.java
@@ -2,16 +2,25 @@ import java.util.Scanner; // Importando Scanner
 
 public class Main {
 	public static void main(String[] args) {
+			
+		Scanner scanner = new Scanner(System.in);//Cria um objeto Scanner	
+		String senha = "";//Declarando variável
+			
+		while (senha.equals("1") != true) { //Loop para programa ficar rodando
+			System.out.println("Digite a senha: (Digite 1 para finalizar)");
+			senha = scanner.next(); //Recebe a senha digitada
+			System.out.println("Falta/Faltam " + contarCaracteres(senha) + " caractere(es) para a senha ser segura"); 
+			// Imprime a quantidade de caracteres faltando para a senha ser segura
+		}
+		scanner.close();//Fecha o scanner
+		System.out.println("Programa finalizado! ");
 		
+	}
+
+	public static int contarCaracteres(String senha) {
 		int qt = 0,tam, i; //Declarando variáveis inteiras
 		boolean maiusculo = false, minusculo = false; // Declarando variáveis booleanas 
 		
-		Scanner scanner = new Scanner(System.in);//Cria um objeto Scanner	
-		
-		System.out.println("Digite a senha: ");
-		String senha = scanner.next(); //Recebe a senha digitada
-		scanner.close();//Fecha o scanner
-		    
 		tam = senha.length(); // Recebe o tamanho da senha digitda
 		
 		for (i = 0; i < tam; i++) {// "For" percorrendo a senha
@@ -43,18 +52,11 @@ public class Main {
 			
 		}
 		
-		if(tam >= 6) {//Verifica o tamanho da senha
-			qt = 4 - qt; // Cálculo para verificar quantos caracteres faltam para a senha ser segura
-			System.out.println("Falta/Faltam " + qt + " caractere(es) para a senha ser segura" ); 
-			// Imprime a quantidade de caracteres que faltam para a senha ser segura
-			
-		}else {
-			qt = qt + 6 - (qt + tam); // Cálculo para verificar quantos caracteres faltam para a senha ser segura, caso a senha tenha menos que 6 caracteres
-			System.out.println("Falta/Faltam " + qt + " caractere(es) para a senha ser segura"); 
-			//Imprime a quantidade de caracteres que faltam para a senha ser segura
-			
-		}
-	
+		qt = 4 - qt; //Cálculo para saber quantos critérios não foram atendidos
+		int temp = Math.max(0, 6 - qt - tam); // Declaração de variável temporária para cálculo de posições não preenchidas
+		qt += temp; // Valor final da quantidade de caracteres faltando para a senha ser segura
+		
+		return qt; // Retornando a quantidade de caracteres faltando para a senha ser segura
 	}
-	    
+    
 }
