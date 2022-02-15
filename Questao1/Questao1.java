61fcfa00f30dec14937d78f298d5d5d31672d1c7
Kaio Yukio <34402495+yukiokaio@users.noreply.github.com>
2022-02-15 13:51:02 -0300

    Update Questao1.java

--- Questao1/Questao1.java
+++ Questao1/Questao1.java
@@ -1,26 +1,25 @@
-import java.util.Scanner;
+import java.util.Scanner; // Importando Scanner
 
 public class Questao1 {
 	public static void main(String[] args) {
 		
-		Scanner scanner = new Scanner(System.in);
-		
-		int i, j, k;
+		Scanner scanner = new Scanner(System.in); //Cria um objeto Scanner
+		int i, j, k; // Declarando variáveis inteiras
 		System.out.println("Digite a quantidade de asteriscos: ");
-	    int qt = scanner.nextInt(); //Recebe a quantidade de asteriscos 
-	    scanner.close(); // Fecha o scanner
+	    	int qt = scanner.nextInt(); //Recebe a quantidade de asteriscos 
+	    	scanner.close(); // Fecha o scanner
 	    
-	    for (i = 1; i <= qt; i++) { //'for' começando de 1 até qt
-	        for (j = qt; j >= 1; j--) {//'for' começando de qt até 1 
-	        	if (j == i) {//Verificando se j é igual a i naquela posição 
-	        		for (k = i; k >= 1; k--) { //"for" para imprimir a quantidade de asteriscos daquela posição
-	        			System.out.print("*");// Imprime asteriscos
-	        		}
-	            } else if (j != i) { //Verificando se j é diferente de i naquela posição e dando espaços
-	                System.out.print(" "); // Dá espaços
-	            }
-	        }
-	        System.out.println(); // Pula linha
-	    }
+	    	for (i = 1; i <= qt; i++) { //'for' começando de 1 até qt
+			for (j = qt; j >= 1; j--) {//'for' começando de qt até 1 
+				if (j == i) {//Verificando se j é igual a i naquela posição 
+					for (k = i; k >= 1; k--) { //"for" para imprimir a quantidade de asteriscos daquela posição
+						System.out.print("*");// Imprime asteriscos
+					}
+			   	 } else if (j != i) { //Verificando se j é diferente de i naquela posição e dando espaços
+					System.out.print(" "); // Dá espaços
+			    }
+			}
+			System.out.println(); // Pula linha
+		    }
 	}
 }
